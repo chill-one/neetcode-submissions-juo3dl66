3246f1ae2bc130e5db7a2a07261d5ef6f9559f09
Suprim Bhandari <106773626+chill-one@users.noreply.github.com>
2026-05-09 19:15:26 -0400

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        """
+                 []
+            [1,]1     []1
+    [1, 2,]2     [1,]2
+[1,2,3]3[1, 2]3 [1,3]3 [1,]3
+        """
+        res = []
+
+        def dfs(curr, exclude):
+            if len(curr) == len(nums):
+                res.append(curr)
+                return
+
+            for i in range(len(exclude)):
+                if not exclude[i]:
+                    curr.append(nums[i])
+                    exclude[i] = True
+                    dfs(curr[:], exclude)
+
+                    exclude[i] = False
+                    curr.pop()
+        dfs([], [False] * (len(nums)))
+        return res
+
+
