634cc58beefbe6288cee2a9aa4b9666b4a95e25f
Suprim Bhandari <106773626+chill-one@users.noreply.github.com>
2026-05-16 12:39:13 -0400

    Add: merge-intervals - submission-7

---
+++ Data Structures & Algorithms/merge-intervals/submission-7.py
@@ -0,0 +1,24 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        #Sort by endTime
+        intervals.sort(key= lambda x: x[0])
+
+
+        res = [intervals[0]]
+
+        #We start the itteration after the first interval
+        for idx in range(1, len(intervals)):
+            start, end = intervals[idx]
+
+            #If the current start time is less than the previous end time its a overlape
+            if res[-1][1] >= start:
+                res[-1][0] = min(start, res[-1][0])
+                res[-1][1] = max(end, res[-1][1])
+            else:
+                res.append(intervals[idx])
+
+
+        return res
+
+
+
