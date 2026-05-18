fb73c04e011a384399b1897928410bcb0bce43e6
Suprim Bhandari <106773626+chill-one@users.noreply.github.com>
2026-05-18 19:17:53 -0400

    Add: hand-of-straights - submission-2

---
+++ Data Structures & Algorithms/hand-of-straights/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+    
+        """
+        [1,2,4,2,3,5,3,4]
+        [1,2,2,3,3,4,4,5]
+        """
+
+        freq = Counter(hand)
+        sorted_keys = sorted(freq.keys())
+        group = 0
+
+        for h in sorted_keys:
+            if freq[h] > 0:
+                count_to_remove = freq[h]
+                for i in range(groupSize):
+                    target = h + i
+                    if freq[target] < count_to_remove:
+                        return False
+                    freq[h + i] -= count_to_remove
+
+
+        return True
+
