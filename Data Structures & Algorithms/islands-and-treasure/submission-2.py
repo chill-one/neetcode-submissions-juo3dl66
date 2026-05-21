f5aa00958023ed38a5557a7b232187b54fadfae7
Suprim Bhandari <106773626+chill-one@users.noreply.github.com>
2026-05-21 15:07:29 -0400

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        """
+          [3,  -1, 0,  1],
+          [2, 2, 1, -1],
+          [1,  -1, 2, -1],
+          [0  ,  -1, 3,  4]
+        """
+        M, N = len(grid), len(grid[0])
+        def dfs(i, j, count):
+            if i < 0 or j < 0 or i >= M or j >= N or grid[i][j] == -1 or count > grid[i][j]:
+                return
+
+            grid[i][j] = min(count, grid[i][j])
+
+            dfs(i, j+1, count + 1)
+            dfs(i, j-1, count + 1)
+            dfs(i+1, j, count + 1)
+            dfs(i-1, j, count + 1)
+
+            return 
+
+        for i in range(M):
+            for j in range(N):
+                if grid[i][j] == 0:
+                    dfs(i, j, 0)
+
+        
