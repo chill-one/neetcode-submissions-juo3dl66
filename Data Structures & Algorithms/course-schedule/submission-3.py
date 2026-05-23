6762213dfb82f165825fe4e2f9c7f859442b1683
Suprim Bhandari <106773626+chill-one@users.noreply.github.com>
2026-05-22 22:28:23 -0400

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        """
+        [a, b] where you must take course b first to take a 
+        """
+        adjList = {}
+
+        for i in range(numCourses):
+            adjList[i] = []
+
+        for course, preReq in prerequisites:
+            adjList[course].append(preReq)
+
+        print(adjList)
+
+        def dfs(course, seen):
+            if course in seen:
+                return False
+            seen.add(course)
+            for preReq in adjList[course]:
+                if not dfs(preReq, seen):
+                    return False
+            seen.remove(course)
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i, set()):
+                return False
+
+        return True
+
