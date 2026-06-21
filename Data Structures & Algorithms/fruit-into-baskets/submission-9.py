0dd5219c4432ebc5ef2750218b5a4c7984382db7
Suprim Bhandari <106773626+chill-one@users.noreply.github.com>
2026-06-21 19:47:20 -0400

    Add: fruit-into-baskets - submission-9

---
+++ Data Structures & Algorithms/fruit-into-baskets/submission-9.py
@@ -0,0 +1,17 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        left = 0
+
+        dic = {}
+        count = 0
+        for right, fruit in enumerate(fruits):
+            dic[fruit] = dic.get(fruit, 0) + 1
+            while len(dic) > 2:
+                dic[fruits[left]] -= 1
+                if dic[fruits[left]] == 0:
+                    dic.pop(fruits[left])
+                left += 1
+            
+            count = max(count, right - left + 1)
+
+        return count
