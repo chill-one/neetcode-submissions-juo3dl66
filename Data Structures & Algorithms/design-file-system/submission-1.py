e97757f074c46920ec1809f3519823d77f0563bb
Suprim Bhandari <106773626+chill-one@users.noreply.github.com>
2026-07-21 08:43:51 -0400

    Add: design-file-system - submission-1

---
+++ Data Structures & Algorithms/design-file-system/submission-1.py
@@ -0,0 +1,32 @@
+class FileSystem:
+
+    def __init__(self):
+        self.p = {"/":-1, "":-1}
+        
+
+    def createPath(self, path: str, value: int) -> bool:
+        if path == "" or path == "/" or path in self.p:
+            return False
+
+        split_path = path.split('/')
+        parent = split_path[:-1]
+
+        parent_path = "/".join(parent)
+        print(parent_path)
+        if parent_path not in self.p:
+            return False
+            
+        self.p[path] = value
+
+        return True
+        
+
+    def get(self, path: str) -> int:
+        return self.p[path] if path in self.p else -1
+        
+
+
+# Your FileSystem object will be instantiated and called as such:
+# obj = FileSystem()
+# param_1 = obj.createPath(path,value)
+# param_2 = obj.get(path)
